adcc7a716ae6d7517efb9dc490b650406188e572
marcosfsr <marcosand89@yahoo.com.br>
2014-11-14 23:53:52 -0200

    Editando_from_web
    Adicionei mais um cout, dessa vez pela web

--- Hello/main.cpp
+++ Hello/main.cpp
@@ -6,5 +6,6 @@ int main()
 {
     cout << "Hello world!" << endl;
     cout << "And it was'nt created from me :p" << endl;
+    cout << "BUT, I can use this from the first time, and it's more a test.." << endl;
     return 0;
 }
